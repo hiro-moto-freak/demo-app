43eaad6febc8f4ec97d87fba729a8689a2d56ad6
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-26 10:39:30 +0900

    投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   def new
-    
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root_path
   end
 
+  private
+  def post_params
+    params.require(:post).permit(:title, :text).merge(user_id: current_user.id)
+  end
 end
