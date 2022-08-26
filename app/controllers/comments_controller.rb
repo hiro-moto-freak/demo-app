1f54f40ec107ed2421bffc11957114b56c2ef4d9
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-26 15:43:20 +0900

    投稿作成機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,18 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to post_path(params[:post_id])
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 
   def destroy
