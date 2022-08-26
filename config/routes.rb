ad4e64c3747012a005107b2a1fc563645d882114
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-26 13:09:24 +0900

    コメントコントローラーの作成とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts, only: [:create, :new, :index, :show, :destroy]
+  resources :posts, only: [:create, :new, :index, :show, :destroy] do
+    resources :comments, only: [:new, :create]
+  end
 end
