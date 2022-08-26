2c6523af470b657fb328444b505ae4a463f3a697
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-08-26 16:15:38 +0900

    コメント削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,14 @@ class CommentsController < ApplicationController
     redirect_to post_path(params[:post_id])
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.user_id == current_user.id
+      @comment.destroy
+      redirect_to post_path(params[:post_id])
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :posts, only: [:create, :new, :index, :show, :destroy] do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: [:new, :create, :destroy]
   end
 end
